686984fc3f3f66bec5f5bb8af681ca502faaeb02
Jia11212 <jiamchen11212@gmail.com>
2023-07-05 22:01:09 -0400

    Updated Row interface and generateRow return value

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -13,10 +13,26 @@ export interface Row {
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
     return serverResponds.map((el: any) => {
+      const price_abc =
+        (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+      const price_def =
+        (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+      const ratio = price_abc / price_def;
+      const lower_bound = 1 - 0.1;
+      const upper_bound = 1 + 0.1;
+
       return {
-        stock: el.stock,
-        top_ask_price: (el.top_ask && el.top_ask.price) || 0,
-        timestamp: el.timestamp,
+        price_abc: price_abc,
+        price_def: price_def,
+        ratio: price_abc / price_def,
+        timestamp:
+          serverResponds[0].timestamp > serverResponds[1].timestamp
+            ? serverResponds[0].timestamp
+            : serverResponds[1].timestamp,
+        lower_bound: lower_bound,
+        upper_bound: upper_bound,
+        trigger_alert:
+          ratio > upper_bound || ratio < lower_bound ? ratio : undefined,
       };
     });
   }
